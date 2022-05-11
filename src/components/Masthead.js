c19b68ec2627feb1c8f50d52a3d157714af58fbe
Noemi <noemialfe95@gmail.com>
2022-05-11 13:54:59 +0200

    masthed - style

--- src/components/Masthead.js
+++ src/components/Masthead.js
@@ -65,7 +65,7 @@ const VerticalLineTop = styled.div`
   margin-left: 50%;
 
   @media (max-width: 441px) {
-    height: 27vh;
+    height: 30vh;
   }
 
 `
@@ -77,7 +77,7 @@ const VerticalLineBottom = styled.div`
   margin-left: 50%;
 
   @media (max-width: 441px) {
-    height: 20vh;
+    height: 25vh;
   }
 `
 
