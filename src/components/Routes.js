353c1fc57e57001abb611403d8b0478e7d3b830d
Noemi <noemialfe95@gmail.com>
2023-11-02 07:01:52 +0100

    navbar R&D update

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,7 +5,7 @@ const Routes = () => {
   return (
     <ul>
       <li><Link to="/">About</Link></li>
-      <li><Link to="/idi">I+D+i</Link></li>
+      <li><Link to="/idi">R&D</Link></li>
       <li><Link to="/sc-communication">Sc. Communication</Link></li>
     </ul>
   )
